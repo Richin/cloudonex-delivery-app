13dba3c26974e2e7690d7fbd927919cd3bc86348
Richin Varghese <richin1990@gmail.com>
2019-10-10 16:02:06 +0530

    token updated

--- www/js/config.js
+++ www/js/config.js
@@ -1,9 +1,9 @@
 /*Driver App Configuration*/
 
-var krms_driver_config ={			
-	'ApiUrl':"",		
+var krms_driver_config ={
+	'ApiUrl':"http://delivery.touchonemobile.akstech.com.sg/driver/api",
 	'DialogDefaultTitle':"DriverApp",
-	'mapboxToken' : '',
-	'APIHasKey':"",
+	'mapboxToken' : 'pk.eyJ1IjoicmljaGludmFyZ2hlc2UiLCJhIjoiY2sxa2s1bGtxMjRiNTNocW1henl4NWZscSJ9.-SeVjZWzZJRrrLlgpTPx8Q',
+	'APIHasKey':"w1o3sdsfvo4eandux30mdr3b70hb4a1fozmd267j",
 	'debug': false
 };
