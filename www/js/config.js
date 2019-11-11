690e34dd2cee04a349dfba10c327d9f091e38dd6
Richin Varghese <richin1990@gmail.com>
2019-11-11 19:41:10 +0530

    mobile city updated

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={
-	'ApiUrl':"http://delivery.touchonemobile.akstech.com.sg/driver/api",
-	'DialogDefaultTitle':"DriverApp",
+	'ApiUrl':"http://delivery.mobilecity-delivery.akstech.com.sg/driver/api",
+	'DialogDefaultTitle':"Mobile City",
 	'mapboxToken' : '',
 	'APIHasKey':"w1o3sdsfvo4eandux30mdr3b70hb4a1fozmd267j",
 	'debug': false
