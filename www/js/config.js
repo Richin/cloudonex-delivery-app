200fcdc2ec40d9889b1db2cf9b2051bed65c2bc8
Richin Varghese <richin1990@gmail.com>
2020-01-16 09:07:22 +0530

    debug true

--- www/js/config.js
+++ www/js/config.js
@@ -5,5 +5,5 @@ var krms_driver_config ={
 	'DialogDefaultTitle':"DriverApp",
 	'mapboxToken' : 'pk.eyJ1IjoicmljaGludmFyZ2hlc2UiLCJhIjoiY2sxa2s1bGtxMjRiNTNocW1henl4NWZscSJ9.-SeVjZWzZJRrrLlgpTPx8Q',
 	'APIHasKey':"w1o3sdsfvo4eandux30mdr3b70hb4a1fozmd267j",
-	'debug': false
+	'debug': true
 };
